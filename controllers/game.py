465ee8db71f913853a75a6bac20f6a1a193aafae
SofyaShirokova <94844730+SofyaShirokova@users.noreply.github.com>
2024-07-08 01:03:12 +1000

    Поймали чечен и русский золотую рыбку. Рыбка говорит: - у Вас по одному желанию. Чечен, долгая выёбываясь, говорит: -хАчу Штоб вокруг чечни стена была высокая-высокая, и что бы не окон не дверей, ни щелочки не было. Штобы ни кто не войти не выйти не мог. Рыбка сделала. Русский спрашивает у рыбки: - стена высокая? - да. -и что, даже щелки нет? - да - Ну тогда заливай цементом!

--- controllers/game.py
+++ controllers/game.py
@@ -1,18 +1,7 @@
 from app import app
-from flask import render_template, session, redirect, url_for, request, flash
-from models.main_page_model import get_profile
-from utils import get_db_connection
+from flask import render_template
 
 
 @app.route('/game')
 def game():
-    conn = get_db_connection()
-
-    profile = get_profile(conn, session.get('username'))
-    if profile.to_string(header=False, index=False) == 'Admin':
-        html = render_template('admin/game.html')
-        if profile.to_string(header=False, index=False) == 'Teacher':
-            html = render_template('teacher/game.html')
-    else:
-        html = render_template('student/game.html')
-    return html
+    return render_template('game/index.html')
